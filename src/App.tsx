02c5fe1133b90c11a9630622abde95cdfb34895e
Pratyush005 <pandapratyush005@gmail.com>
2025-06-13 15:11:10 +0530

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { StatCard } from './components/StatCard';
 import { SimpleBarChart, SimplePieChart } from './components/Chart';
 import { PatientTable } from './components/PatientTable';
